4377988b710949fdcd4e7d6c05434c9823463f15
Martin Atkins <mart@degeneration.co.uk>
2013-08-04 14:30:29 -0700

    Manual dry-run build of an avr cross-toolkit

---
+++ lib/gcc/avr/4.8.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.8.1/configure --prefix=/tmp/gcc --target=avr --enable-languages=c --disable-nls --disable-libssp --with-dwarf2";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
