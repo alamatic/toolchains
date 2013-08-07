f639960d356b0c1b67fcb3de8ad97465b6ad6cdf
Martin Atkins <mart@degeneration.co.uk>
2013-08-06 20:50:25 -0700

    Import hand-built bare-metal arm toolchain

---
+++ lib/gcc/arm-none-eabi/4.8.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../gcc-4.8.1/configure --target=arm-none-eabi --prefix=/tmp/arm-none-eabi --enable-interwork --enable-multilib --enable-languages=c --with-newlib --with-headers=../../newlib-2.0.0/newlib/libc/include";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
